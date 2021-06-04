635f74204cc35890089b1b81ab03f4ce40c41fda
eduardomegha <jeduardosa86@gmail.com>
2021-06-03 21:25:05 -0300

    App concluído

--- app/src/main/java/learn/exercicio/appsorteio/MainActivity.kt
+++ app/src/main/java/learn/exercicio/appsorteio/MainActivity.kt
@@ -2,13 +2,22 @@ package learn.exercicio.appsorteio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+  //My first kotlin app
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+
     }
 
+    fun sortearNumero(view: View) {}
+
+        var texto = findViewById<TextView>(R.id.textoSorteio)
+        var numeroSorteado = Random().nextInt(11)
+        texto.text = "Número sorteado é: " + numeroSorteado
 }
